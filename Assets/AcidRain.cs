d204d1cea457c0581dd56f8c3e922d5286511030
DinoSage <dinosage5632@gmail.com>
2024-09-09 21:46:01 -0400

    Instantiate, Destroy, Layers, and Coroutines Added

---
+++ Assets/AcidRain.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AcidRain : MonoBehaviour
+{
+    [SerializeField]
+    GameObject acidPrefab;
+
+    [SerializeField]
+    float radius;
+
+    [SerializeField]
+    float delay;
+
+    [SerializeField]
+    float cooldown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    IEnumerator Spawn()
+    {
+        while (true)
+        {
+            Vector3 spawnerPos = this.gameObject.transform.position;
+            float randX = Random.Range(spawnerPos.x - radius, spawnerPos.x + radius);
+            float randZ = Random.Range(spawnerPos.z - radius, spawnerPos.z + radius);
+
+            Vector3 acidPos = new Vector3(randX, spawnerPos.y, randZ);
+            GameObject acid = Instantiate(acidPrefab, acidPos, Quaternion.identity);
+            Destroy(acid, delay);
+            yield return new WaitForSeconds(cooldown);
+        }
+    }
+}
